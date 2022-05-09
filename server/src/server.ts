6d468945f83fa70eaadbb0d4c6d482c56aef809a
Everton Pavan <everton.pavan.dev@gmail.com>
2022-05-09 13:32:28 -0300

    fix: message server running

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,5 +10,5 @@ app.use(routes);
 
 
 app.listen(process.env.PORT || 3333, () => {
-    console.log('Server started on port 3333');
+    console.log(`Server started on port ${process.env.PORT}`);
 });
